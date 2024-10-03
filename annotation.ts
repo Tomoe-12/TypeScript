2f272fec9da4ac4686fb16902907863b45af5c3f
Tomoe-12 <li8993han@gmail.com>
2024-10-04 05:23:23 +0630

    type annotation

---
+++ annotation.ts
@@ -0,0 +1,34 @@
+// string
+let fruit: string = 'apple'
+// fruit = 20    error
+
+// number
+let age: number = 20;
+// age = 'sttile' error
+
+// boolean
+let isLogin: boolean = true
+// isLogin =20  error
+
+// undefined
+let username: undefined = undefined
+
+
+let email: null = null
+
+// string array
+let fruits: string[] = ['apples', 'orange', 'banana']
+
+// number array
+let ages: number[] = [12, 22, 30]
+
+// boolean array 
+let random: boolean[] = [true, false, true, true, false]
+
+
+// objects
+let person: { name: string; age: number; isPass: boolean } = {
+    name: 'thithi',
+    age: 20,
+    isPass: true,
+}
