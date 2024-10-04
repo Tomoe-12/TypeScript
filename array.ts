fdd6a71f4c4fe6f371cc5b5d1f362e52f59e959c
Tomoe-12 <li8993han@gmail.com>
2024-10-04 16:57:55 +0630

    nested and flex array

---
+++ array.ts
@@ -0,0 +1,15 @@
+// array
+
+// nested array
+let nestArr:
+    string[][]
+    =
+    [
+        ["arr1"],
+        ["arr2"],
+        ["arr3"]
+    ]
+
+    
+// flex array
+let flexArr: (string | number | boolean)[] = ["thithi", 20, 'apple', 30, true]
