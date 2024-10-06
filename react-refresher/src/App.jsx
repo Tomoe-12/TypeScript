b2afb5548ad079ce205beb54de995e76ba4afcdd
Tomoe-12 <li8993han@gmail.com>
2024-10-06 21:14:02 +0630

    useEffect

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,25 +1,42 @@
-import { useState } from 'react'
-import CustomText from './components/CustomText.jsx'
+import { useEffect, useState } from "react";
+import CustomText from "./components/CustomText.jsx";
 
 function App() {
-  const [text, setText] = useState('hello')
+  const [text, setText] = useState("hello");
 
   const inputHandler = (e) => {
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
 
   const clearVal = () => {
-    setText('')
-  }
+    setText("");
+  };
+
+
+  useEffect(() => {
+    console.log('i am here');
+    
+  }, [text])
+  
+
   return (
     <>
       <CustomText text={text} /> <br />
       <CustomText text={text} /><br />
       <CustomText text={text} /><br />
-      input feld ::  <input className='border border-black' type="text" value={text} onChange={(e) => inputHandler(e)} /> <br />
-      <button className='border border-red-400' onClick={clearVal}>Set to empty button </button>
+      input feld ::{" "}
+      <input
+        className="border border-black"
+        type="text"
+        value={text}
+        onChange={(e) => inputHandler(e)}
+      />{" "}
+      <br />
+      <button className="border border-red-400" onClick={clearVal}>
+        Set to empty button{" "}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
