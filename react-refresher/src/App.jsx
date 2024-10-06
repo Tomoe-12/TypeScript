a62747c8db3a4601ff76f2c7e55374917002d738
Tomoe-12 <li8993han@gmail.com>
2024-10-06 21:01:44 +0630

    events

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react'
 
-import './App.css'
 
 function App() {
+  const [text, setText] = useState('hello')
 
+  const inputHandler = (e) =>{
+    setText(e.target.value)
+  }
+
+  const clearVal = (e) =>{
+    setText('')
+  }
   return (
     <>
-      <h1 className='text-red-500'>hello world</h1>
+      <h1 className='text-red-500'>{text}</h1>
+     input feld ::  <input className='border border-black' type="text" value={text} onChange={(e)=>inputHandler(e)} />
+     <h1 className='text-red-500'>{text}</h1>
+     <button className='border border-red-400' onClick={(e) => clearVal(e)}>Set to empty button </button>
     </>
   )
 }
