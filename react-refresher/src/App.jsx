d7d6c71791821063a6c1a0ac564f32543ab0a380
Tomoe-12 <li8993han@gmail.com>
2024-10-06 21:37:48 +0630

    dummy project

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,42 +1,38 @@
-import { useEffect, useState } from "react";
-import CustomText from "./components/CustomText.jsx";
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [text, setText] = useState("hello");
+const App = () => {
+  const [todos, setTodos] = useState([])
 
-  const inputHandler = (e) => {
-    setText(e.target.value);
-  };
+  const fetchTodos = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await res.json()
+    setTodos(data)
 
-  const clearVal = () => {
-    setText("");
-  };
-
-
+  }
   useEffect(() => {
-    console.log('i am here');
-    
-  }, [text])
-  
-
+    fetchTodos()
+  }, [])
   return (
-    <>
-      <CustomText text={text} /> <br />
-      <CustomText text={text} /><br />
-      <CustomText text={text} /><br />
-      input feld ::{" "}
-      <input
-        className="border border-black"
-        type="text"
-        value={text}
-        onChange={(e) => inputHandler(e)}
-      />{" "}
-      <br />
-      <button className="border border-red-400" onClick={clearVal}>
-        Set to empty button{" "}
-      </button>
-    </>
-  );
+    <section className='w-1/2 mx-auto'>
+      <div>
+        <div>
+          <h1 className='text-5xl font-bold text-center my-4 uppercase'>React Exercises</h1>
+          <input type="text" className='border border-black focus:none rounded-md p-1 w-3/4' />
+          <button className='bg-black text-white p-2 rounded ms-3 text-sm'>search</button>
+        </div>
+        <div className='grid grid-cols-2 gap-3 mt-6'>
+          {
+            todos.map((todo, i) => (
+              <div key={i} className='bg-black text-white p-3 rounded'>
+                <h3>{todo.title}</h3>
+                <p>status : {todo.completed ? 'completed' : 'progress'}</p>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
